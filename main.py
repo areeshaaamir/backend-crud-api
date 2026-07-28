721cd0360e325186334c01ae8c7c5387e98aa682
areeshaaamir <f2023065219@umt.edu.pk>
2026-07-28 16:42:59 +0500

    read endpoints with 404

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+tasks = [
+    {
+        'id': 1,
+        'title': "Complete the FlyRank AI Assignment",
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': "Farm 1000 Primogems in Genshin",
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': "Make dinner",
+        'done': False
+    }
+]
 
 @app.get("/")
 def root():
@@ -13,3 +31,19 @@ def root():
 @app.get("/health")
 def getHealth():
     return {'status':"OK"}
+
+@app.get("/tasks")
+def getTasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_one_task(task_id: int):
+    
+    for task in tasks:
+        if task_id == task["id"]:
+            return task
+        
+    raise HTTPException(
+        status_code= 404,
+        detail= f"Task {task_id} does not exist"
+    )
