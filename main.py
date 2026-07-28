55b3fc3a85b0e7826a286adf57096d4e740284a3
areeshaaamir <f2023065219@umt.edu.pk>
2026-07-28 16:20:52 +0500

    Stage 1: Root and health endpoints

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-
 def root():
-    return {'message': "CRUD API is Running"}
+    return {
+        'name':'CRUD Task API',
+        'version':'1.0',
+        'endpoints':["/tasks"]
+    }
+    
+@app.get("/health")
+def getHealth():
+    return {'status':"OK"}
